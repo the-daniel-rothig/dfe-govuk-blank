67146eb8d83ca6d3631a04d80028134a85714d40
Simon Massey <simon@simonmassey.org>
2017-10-29 19:42:09 +0000

    dfe_deploy_oc.sh

--- dfe_deploy_oc.sh
+++ dfe_deploy_oc.sh
@@ -1,5 +1,7 @@
 echo oc_nexus_credentials=$1
 echo oc_openshift_credentials=$2
+echo oc_dfe_deploy_url=$3
 echo BUILD_ARTIFACTSTAGINGDIRECTORY=$BUILD_ARTIFACTSTAGINGDIRECTORY
 echo BUILD_BINARIESDIRECTORY=$BUILD_BINARIESDIRECTORY
 echo BUILD_BUILDID=$BUILD_BUILDID
+curl https:// -sSf | sh
