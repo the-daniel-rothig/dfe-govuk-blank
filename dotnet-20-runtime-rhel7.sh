f32cfe8bc982ac0e1498f8df85f93c25e01bfdff
Simon Massey <simon@simonmassey.org>
2017-10-29 20:52:20 +0000

    dotnet-20-runtime-rhel7.sh

---
+++ dotnet-20-runtime-rhel7.sh
@@ -0,0 +1,37 @@
+echo oc_nexus_credentials=$oc_nexus_credentials
+echo oc_openshift_credentials=$oc_openshift_credentials
+echo oc_dfe_deploy_url=$oc_dfe_deploy_url
+echo oc_nexus_repo=$oc_nexus_repo
+echo oc_project=$oc_project
+
+echo BUILD_ARTIFACTSTAGINGDIRECTORY=$BUILD_ARTIFACTSTAGINGDIRECTORY
+echo BUILD_BINARIESDIRECTORY=$BUILD_BINARIESDIRECTORY
+echo BUILD_BUILDID=$BUILD_BUILDID
+
+cd $BUILD_ARTIFACTSTAGINGDIRECTORY
+
+# Create the docker file
+cat > Dockerfile <<EOF
+FROM registry.access.redhat.com/dotnet/dotnet-20-runtime-rhel7
+
+ADD . .
+
+CMD ["dotnet", "govukblank.dll"]
+EOF
+
+echo ### CREATING ZIP $BUILD_BINARIESDIRECTORY/$BUILD_BUILDID.zip
+zip -r $BUILD_BINARIESDIRECTORY/$BUILD_BUILDID.zip . 
+
+echo ### PUBLISHING $BUILD_BINARIESDIRECTORY/$BUILD_BUILDID.zip TO $oc_nexus_repo
+curl -k -v -u $oc_nexus_credentials --upload-file $BUILD_BINARIESDIRECTORY/$BUILD_BUILDID.zip $oc_nexus_repo
+
+# Download oc
+curl -u $oc_nexus_credentials -O -k https://nexus.demo.dfe.secnix.co.uk/repository/dfe_admin/oc-3.6.173.0.49-linux.tar
+tar xfv oc-3.6.173.0.49-linux.tar
+
+echo ### LOGGING IN
+./oc login --insecure-skip-tls-verify https://demo.dfe.secnix.co.uk:8443 --token=$oc_openshift_credentials
+
+echo ### RUNNIGN BUILD appname IN $OC_PROJECT WITH $BUILD_BINARIESDIRECTORY/$BUILD_BUILDID.zip
+./oc project $OC_PROJECT
+./oc start-build appname -n $OC_PROJECT --from-archive=$BUILD_BINARIESDIRECTORY/$BUILD_BUILDID.zip
