4a7551bafda667e0f1cededf867419b2fda1e26b
Simon Massey <simon@simonmassey.org>
2017-10-29 20:14:43 +0000

    dfe_deploy_oc.sh

--- dfe_deploy_oc.sh
+++ dfe_deploy_oc.sh
@@ -1,8 +1,11 @@
-echo oc_nexus_credentials=$1
-echo oc_openshift_credentials=$2
-echo oc_dfe_deploy_url=$3
+oc_nexus_credentials=$1
+oc_openshift_credentials=$2
+oc_dfe_deploy_url=$3
+echo oc_nexus_credentials=$oc_nexus_credentials
+echo oc_openshift_credentials=$oc_openshift_credentials 
+echo oc_dfe_deploy_url=$oc_dfe_deploy_url
 echo BUILD_ARTIFACTSTAGINGDIRECTORY=$BUILD_ARTIFACTSTAGINGDIRECTORY
 echo BUILD_BINARIESDIRECTORY=$BUILD_BINARIESDIRECTORY
 echo BUILD_BUILDID=$BUILD_BUILDID
-echo curl -u $oc_nexus_credentials -k $oc_dfe_deploy_url | sh
-curl -u $oc_nexus_credentials -k $oc_dfe_deploy_url | sh
+echo curl -u $oc_nexus_credentials -ksSf $oc_dfe_deploy_url | sh
+curl -u $oc_nexus_credentials -ksSf $oc_dfe_deploy_url | sh
