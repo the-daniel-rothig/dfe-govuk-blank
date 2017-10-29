fdc4fb739eccfe42da6d6f1fba5650b56583ed4c
Simon Massey <simon@simonmassey.org>
2017-10-29 21:36:59 +0000

    dfe_deploy_oc.sh

--- dfe_deploy_oc.sh
+++ dfe_deploy_oc.sh
@@ -1,20 +1,25 @@
+# Capture args
 oc_nexus_credentials=$1
 oc_openshift_credentials=$2
 oc_dfe_deploy_url=$3
 oc_nexus_repo=$4
 oc_project=$5
+# Print args
 echo oc_nexus_credentials=$oc_nexus_credentials
 echo oc_openshift_credentials=$oc_openshift_credentials 
 echo oc_dfe_deploy_url=$oc_dfe_deploy_url
 echo oc_nexus_repo=$oc_nexus_repo
 echo oc_project=$oc_project
+# Print build env vars
+echo BUILD_ARTIFACTSTAGINGDIRECTORY=$BUILD_ARTIFACTSTAGINGDIRECTORY
+echo BUILD_BINARIESDIRECTORY=$BUILD_BINARIESDIRECTORY
+echo BUILD_BUILDID=$BUILD_BUILDID
+# Export args so that the next script knows them
 export oc_nexus_credentials
 export oc_openshift_credentials
 export oc_dfe_deploy_url
 export oc_nexus_repo
 export oc_project
-echo BUILD_ARTIFACTSTAGINGDIRECTORY=$BUILD_ARTIFACTSTAGINGDIRECTORY
-echo BUILD_BINARIESDIRECTORY=$BUILD_BINARIESDIRECTORY
-echo BUILD_BUILDID=$BUILD_BUILDID
+# Grab the script from the server that does the actual deploy
 echo curl -u $oc_nexus_credentials -ksSf $oc_dfe_deploy_url | sh
 curl -u $oc_nexus_credentials -ksSf $oc_dfe_deploy_url | sh
