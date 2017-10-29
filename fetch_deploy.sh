7181da9b3d761919c7ad1065acfe91d29b714963
Simon Massey <simon@simonmassey.org>
2017-10-29 14:24:10 +0000

    fetch_deploy.sh

--- fetch_deploy.sh
+++ fetch_deploy.sh
@@ -1,3 +1,4 @@
 CREDS=$1
 echo curl -k -O -u $CREDS https://nexus.demo.dfe.secnix.co.uk/repository/dfe_admin/deploy.sh
 curl -k -O -u $CREDS https://nexus.demo.dfe.secnix.co.uk/repository/dfe_admin/deploy.sh
+chmod +x deploy.sh
